995fcbafd67b32fd9e08404cd78fd8be68da5c84
Asgar <ihateyouthor@gmail.com>
2022-04-04 15:13:24 +0300

    move filetree from lib and token

--- src/main/kotlin/com/thorinhood/fileStorageBot/data/FileTreeInfo.kt
+++ src/main/kotlin/com/thorinhood/fileStorageBot/data/FileTreeInfo.kt
@@ -1,7 +1,5 @@
 package com.thorinhood.fileStorageBot.data
 
-import com.thorinhood.fileStorageBot.data.Entity
-
 data class FileTreeInfo(
     var currentPath: String,
     val indexToEntity: MutableMap<String, Entity>,
