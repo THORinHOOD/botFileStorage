ce8e6d5d8123aa83aa245e895e08d9777e5dcddb
Asgar <ihateyouthor@gmail.com>
2022-04-16 17:37:39 +0300

    refactoring

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
 rootProject.name = "fileBot"
-
-include("chatBotEngine")
--- src/main/kotlin/com/thorinhood/fileStorageBot/FileStorageBotApplication.kt
+++ src/main/kotlin/com/thorinhood/fileStorageBot/FileStorageBotApplication.kt
@@ -1,20 +1,12 @@
 package com.thorinhood.fileStorageBot
 
-import org.springframework.boot.SpringBootConfiguration
-import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
-import org.springframework.boot.context.TypeExcludeFilter
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.FilterType
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication(
 	exclude = [MongoAutoConfiguration::class]
 )
-//@EnableMongoRepositories
-//@Import(value = [BenchLibClientsConfig::class, BenchLibUtilsConfig::class])
 class FileStorageBotApplication
 
 fun main(args: Array<String>) {
